1233c79ae85293114e14c625bf8db6835381a996
Kacper Jach <kacperini@gmail.com>
2022-01-26 16:17:07 +0100

    api setup
    laravel package for tmdb + api setup. pls be working.

---
+++ config/tmdb.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package php-tmdb\laravel
+ * @author Mark Redeman <markredeman@gmail.com>
+ * @copyright (c) 2014, Mark Redeman
+ */
+return [
+    /*
+     * Api key
+     */
+    'api_key' => '57a2ee4b8fcf5ed195a941d8ebe7f588',
+];
