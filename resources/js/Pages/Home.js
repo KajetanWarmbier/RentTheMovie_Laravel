ccc4fbc97436d50e01715a49739cea1ef9dca859
Kajetan Warmbier <kajetan.warmbier@gmail.com>
2022-01-26 23:03:57 +0100

    Working HOME

--- resources/js/Pages/Home.js
+++ resources/js/Pages/Home.js
@@ -1,55 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, Head } from "@inertiajs/inertia-react";
 import Layout from "@/Layouts/Layout";
 import MovieCard from "@/Components/MovieCard";
 
 const Home = (props) => {
     console.log(props.popular);
+
+    const [pupularMoviesList, setPopularMoviesList] = useState([]);
+
+    useEffect(() => {
+        setPopularMoviesList(props.popular);
+    }, []);
+
     return (
         <div className="h-screen overflow-auto pb-[200px] scrollbar-hide">
             <div className="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 justify-items-center gap-x-5 gap-y-12">
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
-                <MovieCard
-                    posterPath="/jKBjeXM7iBBV9UkUcOXx3m7FSHY.jpg"
-                    movieName="Batman"
-                    rating="80"
-                    id="id"
-                />
+                {pupularMoviesList.map((movie) => {
+                    return <MovieCard movieData={movie} />;
+                })}
             </div>
         </div>
     );
