7f9b1c99ffce85c267f0c5e7fcb8c88efdfabbb5
Kajetan Warmbier <kajetan.warmbier@gmail.com>
2022-01-27 17:32:20 +0100

    Added profile functionality in mobile + prof. page

---
+++ resources/js/Components/CategoriesModalMobile.js
@@ -0,0 +1,9 @@
+const CategoriesModalMobile = () => {
+    return (
+        <div className="group flex items-center px-4 py-3 text-xl font-semibold border-b-2 border-white border-opacity-30  text-white backdrop-filter-none backdrop-blur-none">
+            CATEGORIES
+        </div>
+    );
+};
+
+export default CategoriesModalMobile;
