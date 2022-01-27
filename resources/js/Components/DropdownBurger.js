a3b77276b51e663f9ecc8f55dc335a8e11908f4d
Kajetan Warmbier <kajetan.warmbier@gmail.com>
2022-01-27 22:31:42 +0100

    SearchBar

--- resources/js/Components/DropdownBurger.js
+++ resources/js/Components/DropdownBurger.js
@@ -34,7 +34,7 @@ const DropdownBurger = () => {
                             </Link>
                         </Menu.Item>
                         <Menu.Item>
-                            <Link href="/shelf">
+                            <Link href="/categoriesmobile">
                                 <div className="group flex items-center px-4 py-3 text-xl font-semibold border-b-2 border-white border-opacity-30  text-white backdrop-filter-none backdrop-blur-none">
                                     CATEGORIES
                                 </div>
